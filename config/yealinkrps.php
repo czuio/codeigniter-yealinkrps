ec90aa079add999e627fbe09827501f0b16c9647
root <root@tftp01.yscb01.au.vertiro.co>
2015-12-12 14:12:18 +1100

    Initial Commit, Basic Framework.

---
+++ config/yealinkrps.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Yealink RPS Username & Password 
+|--------------------------------------------------------------------------
+|
+|
+*/
+$config['rps_username'] = ''; // Your username here
+$config['rps_password'] = ''; // Your password here
